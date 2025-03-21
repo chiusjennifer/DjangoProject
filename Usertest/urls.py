2dfae55e6090159653180e974bf7ad7c32405b13
Jennifer <chiusjennifer@gmail.com>
2025-03-21 18:08:33 +0800

    登入註冊

---
+++ Usertest/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for Usertest project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+
+from django.contrib.auth.views import LoginView
+
+from user.views import index_page, login_page, register_page, logout_page
+
+app_name = 'users'
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("",index_page, name="index"),
+    path("login/",login_page,name="login"),
+    path("register/",register_page,name="register"),
+    path("logout",logout_page,name="logout"),
+]
---
+++ user/views.py
@@ -0,0 +1,50 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from user.forms import CustomUserForm
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+
+
+def index_page(request):
+    return render(request, 'users/index.html')
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        print(f"Username: {username}, Password: {password}")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+    context = {}
+    return render(request, 'users/login.html', context)
+
+
+def register_page(request):
+    if request.method != 'POST':
+        form = CustomUserForm()
+    else:
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
